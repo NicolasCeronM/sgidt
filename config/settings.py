27a7b861a1e0c178c55ccf72bface38ebc27927f
NicolasCeronM <nicolas134b@gmail.com>
2025-08-22 23:15:24 -0400

    Dockerizar el proyecto con Django y PostgreSQL (configuración básica)

--- config/settings.py
+++ config/settings.py
@@ -121,10 +121,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 # -------------------------------------------------------------------
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "sgidt",
+        "USER": "sgidt_user",
+        "PASSWORD": "admin123",   # la que pusiste
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
